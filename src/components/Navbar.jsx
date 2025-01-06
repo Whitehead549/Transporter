ffc05d46f58c1becd8fd090d17f27323c7c044a2
Whitehead549 <finbareric549@gmail.com>
2025-01-06 03:15:37 +0100

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import Hamburger from "./popups/Hamburger";
 
 const Navbar = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
   return (
     <nav className="bg-[#091242] text-white py-8">
       <div className="container mx-auto flex justify-between items-center">
@@ -19,45 +12,39 @@ const Navbar = () => {
           </Link>
         </div>
 
-        {/* Hamburger Icon */}
-        <Hamburger isOpen={isMenuOpen} toggleMenu={toggleMenu} />
-
         {/* Navigation Links */}
-        <ul
-          className={`${
-            isMenuOpen ? "block" : "hidden"
-          } md:flex md:space-x-6 md:static absolute top-full left-0 w-full bg-[#091242] md:bg-transparent p-4 md:p-0`}
-        >
+        <ul className="flex space-x-6">
           <li>
-            <Link to="/" className="hover:text-gray-400 block md:inline">
+            <Link to="/" className="hover:text-gray-400">
               Home
             </Link>
           </li>
           <li>
-            <Link to="/about" className="hover:text-gray-400 block md:inline">
+            <Link to="/about" className="hover:text-gray-400">
               About
             </Link>
           </li>
           <li>
-            <Link to="/services" className="hover:text-gray-400 block md:inline">
+            <Link to="/services" className="hover:text-gray-400">
               Services
             </Link>
           </li>
           <li>
-            <Link to="/payment" className="hover:text-gray-400 block md:inline">
+            <Link to="/payment" className="hover:text-gray-400">
               Make Payment
             </Link>
           </li>
           <li>
-            <Link to="/track" className="hover:text-gray-400 block md:inline">
+            <Link to="/track" className="hover:text-gray-400">
               Track
             </Link>
           </li>
           <li>
-            <Link to="/contact" className="hover:text-gray-400 block md:inline">
+            <Link to="/contact" className="hover:text-gray-400">
               Contact Us
             </Link>
           </li>
+         
         </ul>
       </div>
     </nav>
