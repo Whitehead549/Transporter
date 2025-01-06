0a329432095f3ea00486d17fc85ed4c126f72128
Whitehead549 <finbareric549@gmail.com>
2025-01-06 22:35:04 +0100

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="bg-[#091242] text-white py-8">
+    <nav className="bg-[#091242] text-white py-6">
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo */}
         <div className="text-2xl font-bold">
