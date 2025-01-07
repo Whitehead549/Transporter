531809a5da0c0b35fa84b5e8a6097381df32edc0
Whitehead549 <finbareric549@gmail.com>
2025-01-07 02:28:20 +0100

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,40 @@
-import React from 'react'
-import DeliverIcon from '../components/Essentials/DeliverIcon'
-import Hero from '../components/Hero'
-import Logistics from '../components/Logistics'
-import Support from '../components/Support'
-import Steps from '../components/Steps'
-import Testimonial from '../components/Essentials/Testimonial'
-import DailyStats from '../components/Essentials/DailyStats'
-import PerService from '../components/Essentials/PerService'
-import Partners from '../components/Partners'
-
-
+import React, { useState, useEffect } from 'react';
+import DeliverIcon from '../components/Essentials/DeliverIcon';
+import Hero from '../components/Hero';
+import Logistics from '../components/Logistics';
+import Support from '../components/Support';
+import Steps from '../components/Steps';
+import Testimonial from '../components/Essentials/Testimonial';
+import DailyStats from '../components/Essentials/DailyStats';
+import PerService from '../components/Essentials/PerService';
+import Partners from '../components/Partners';
 
 const Home = () => {
+  const [showHero, setShowHero] = useState(false);
+
+  useEffect(() => {
+    // Set a timeout to display the Hero section after 0.5 seconds
+    const timer = setTimeout(() => {
+      setShowHero(true);
+    }, 500);
+
+    // Cleanup the timer
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className='bg-gray-100 overflow-x-hidden'>
-      <Hero />
-      <Logistics/>
+      {showHero ? <Hero /> : null}
+      <Logistics />
       <Support />
-      <DeliverIcon/>
-      <Steps/>
+      <DeliverIcon />
+      <Steps />
       <Testimonial />
       <DailyStats />
       <PerService />
       <Partners />
-     
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
