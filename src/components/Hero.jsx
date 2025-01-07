4732a0de053e85a68e8a887c180d269fd203506b
Whitehead549 <finbareric549@gmail.com>
2025-01-07 01:50:07 +0100

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <div className="relative w-full h-[30rem] bg-white flex items-center justify-center">
+    <div className="relative w-full h-[30rem] bg-gray-100 flex items-center justify-center">
       {/* Content Wrapper */}
       <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-6 md:px-12 lg:px-20">
         {/* Heading */}
-        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-gray-800 mb-6 leading-tight max-w-3xl tracking-wide">
-          We Are Global Logistic Providers
+        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-custom_blue mb-6 leading-tight max-w-3xl tracking-wide">
+          We Are <span className='text-custom_gold'>Global Logistic Providers</span>
         </h1>
 
         {/* Description */}
-        <p className="text-sm sm:text-base md:text-lg lg:text-xl text-gray-700 max-w-2xl mb-10 px-4 sm:px-8 leading-relaxed">
+        <p className="text-sm sm:text-base md:text-lg lg:text-xl text-custom_blue max-w-2xl mb-10 px-4 sm:px-8 leading-relaxed">
           Specializing in fast, reliable, and global logistics services, we ensure your goods are delivered safely and on time with a network that spans the globe.
         </p>
 
@@ -20,7 +20,7 @@ const Hero = () => {
           {/* Track Item Button */}
           <a
             href="https://freightswiftlogistics.com/tracking/"
-            className="bg-transparent border-2 border-gray-800 text-gray-800 py-3 px-8 sm:px-10 rounded-full hover:bg-gray-800 hover:text-white transition-all duration-300 transform hover:scale-105 shadow-lg"
+            className="bg-custom_blue border-2 border-gray-800 text-white py-3 px-8 sm:px-10 rounded-full hover:bg-gray-800 hover:text-white transition-all duration-300 transform hover:scale-105 shadow-lg"
           >
             Track Item
           </a>
@@ -28,7 +28,7 @@ const Hero = () => {
           {/* About Us Button */}
           <a
             href="https://freightswiftlogistics.com/about/"
-            className="bg-transparent border-2 border-gray-800 text-gray-800 py-3 px-8 sm:px-10 rounded-full hover:bg-gray-800 hover:text-white transition-all duration-300 transform hover:scale-105 shadow-lg"
+            className="bg-custom_blue border-2 border-gray-800 text-white py-3 px-8 sm:px-10 rounded-full hover:bg-gray-800 hover:text-white transition-all duration-300 transform hover:scale-105 shadow-lg"
           >
             About Us
           </a>
