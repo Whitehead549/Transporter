613c0fcb28f8ebc8b23f544068618431b92852ed
Whitehead549 <finbareric549@gmail.com>
2025-03-07 18:00:29 +0100

    first commit

--- src/components/Essentials/Invoice.jsx
+++ src/components/Essentials/Invoice.jsx
@@ -97,13 +97,22 @@ const Invoice = ({ selectedCode }) => {
         ref={invoiceRef}
         className="invoice-container w-full max-w-2xl mx-auto px-4 sm:px-6 py-4 bg-white shadow-lg border border-gray-300 overflow-y-auto"
       >
-        <header className="flex flex-col sm:flex-row justify-between items-start sm:items-center border-b-4 border-blue-800 pb-4 mb-4">
-          <div className="flex-shrink-0">
-            <img src={logo} alt="Company Logo" className="h-auto w-[108px]" />
+       <header className="flex flex-col sm:flex-row justify-between items-start sm:items-center border-b-4 border-blue-800 pb-4 mb-4">
+          {/* Logo Container */}
+          <div className="flex-shrink-0 mb-4 sm:mb-0">
+            <img src={logo} alt="Company Logo" className="h-auto w-[108px] sm:w-[120px] md:w-[140px]" />
           </div>
-          <div className="text-right mt-4 sm:mt-0 text-sm sm:text-md font-semibold text-gray-700 grid gap-1">
-            <p>Tracking Identifier: {selectedCode}</p>
-            <p>Expected Date: {invoiceData.expectedDelivery}</p>
+
+          {/* Invoice Details */}
+          <div className="text-left sm:text-right text-xs sm:text-md lg:text-sm font-semibold text-gray-700 grid gap-2 sm:gap-1">
+            <p className="grid grid-cols-[auto_1fr] gap-4 sm:gap-6">
+              <span className="whitespace-nowrap">Tracking Identifier:</span>
+              <span className="font-normal text-gray-600">{selectedCode}</span>
+            </p>
+            <p className="grid grid-cols-[auto_1fr] gap-4 sm:gap-6">
+              <span className="whitespace-nowrap">Expected Date:</span>
+              <span className="font-normal text-gray-600">{invoiceData.expectedDelivery}</span>
+            </p>
           </div>
         </header>
         <div className="mb-6 border border-gray-300 rounded-lg p-4 bg-gray-50 shadow-sm">
